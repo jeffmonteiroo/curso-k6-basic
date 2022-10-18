527bc35f7f2698ee918a53c71c4cfbe1687c5368
Jeff Monteiro <jeffmonteiroo@gmail.com>
2022-10-18 12:37:19 -0300

    meu primeiro commit

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --request POST \ 
+    --url http://localhost:3333/signup \
+    --header 'Content-Type: application/json' \
+    --data '{"email": "jeffmonteiroo@gmail.com", "password": "pwd123"}'
